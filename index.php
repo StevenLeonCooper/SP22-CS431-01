500abd448e3f7fb7a08364665bae3667abd247bc
Steven Cooper <stevenleoncooper@gmail.com>
2022-02-09 19:49:35 -0800

    Added Demo Files

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Demo Page</title>
+    </head>
+    <body>
+        <h1>Demo For Wednesday</h1>
+        <p>
+            A paragraph of text.
+
+            <?php
+                echo("<!-- This is PHP! -->");
+            ?>
+        </p>
+    </body>
+</html>
