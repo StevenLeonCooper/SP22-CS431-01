0608dc251c50c04989e8b7f2f722da48152735f2
ZachHofmeister <zachhof@me.com>
2022-02-14 17:52:25 -0800

    restructured index.php

--- index.php
+++ index.php
@@ -4,28 +4,23 @@
         <title>Demo Page</title>
     </head>
     <body>
-        <h1>Demo For Wednesday</h1>
-        <p>
-            A paragraph of text.
+        
 
-<<<<<<< HEAD
 	<body>
 		<header>
 
 		</header>
 		<main>
-			
+			<h1>Demo For Wednesday</h1>
+			<p>
+				A paragraph of text.
+				<?php
+                	echo("<!-- This is PHP! -->");
+				?>
+			</p>
 		</main>
 		<footer>
 
 		</footer>
 	</body>
-<html>
-=======
-            <?php
-                echo("<!-- This is PHP! -->");
-            ?>
-        </p>
-    </body>
 </html>
->>>>>>> a79f09e26ebeb7d100fd38a35ed02dfef8f66c2a
