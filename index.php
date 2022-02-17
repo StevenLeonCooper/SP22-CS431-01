e93ab4feaf37285464bd9b53ce7ce0df72de2c54
ZachHofmeister <zachhof@me.com>
2022-02-16 21:22:57 -0800

    added some files

--- index.php
+++ index.php
@@ -2,9 +2,8 @@
 <html lang="en">
     <head>
         <title>Demo Page</title>
+		<link rel="stylesheet" href="css/home.css" type="text/css"/>
     </head>
-    <body>
-        
 
 	<body>
 		<header>
@@ -18,9 +17,27 @@
                 	echo("<!-- This is PHP! -->");
 				?>
 			</p>
+			<button class="say-hi" type="button" data-message="Hello world!!">Hello world</button>
 		</main>
 		<footer>
 
 		</footer>
+
+		<script>
+			// document.querySelectorAll(".say-hi").forEach(button => {
+			// 	button.addEventListener("click", function() {
+			// 		alert("Hello world!");
+			// 	});
+			// });
+
+			document.addEventListener("click", event => {
+				let eventSource = event.target;
+
+				if (!eventSource.classList.contains("say-hi")) return;
+
+				let displayText = eventSource.dataset.message ?? "No message found." //Separate content like strings out of logic - maybe a dictionary for no message found message.
+				alert(displayText);
+			});
+		</script>
 	</body>
 </html>
