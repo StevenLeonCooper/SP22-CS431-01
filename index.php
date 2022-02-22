949dd4a24242da1a34f37248d32b04807040f63e
Jessica Alarcon <Alarconj1498@csu.fullerton.edu>
2022-02-21 19:57:31 -0800

    Done?

--- index.php
+++ index.php
@@ -1,43 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Demo Page</title>
-		<link rel="stylesheet" href="css/home.css" type="text/css"/>
+        <title>CPSC431 Wep App - Phase 1(<?php echo ($page); ?>)</title>
+		<style>
+			<?php include("css/app.css"); ?>
+		</style>
+		<link class="page-css" id="css_<?php echo ($page); ?>" rel="stylesheet" href="css/<?php echo ($page); ?>.css" />
     </head>
 
 	<body>
 		<header>
-
+			<nav>
+				<!-- this header is on every page. -->
+            </nav>
 		</header>
 		<main>
-			<h1>Demo For Wednesday</h1>
-			<p>
-				A paragraph of text.
-				<?php
-                	echo("<!-- This is PHP! -->");
-				?>
-			</p>
-			<button class="say-hi" type="button" data-message="Hello world!!">Hello world</button>
-		</main>
+		    <h1>Demo For Wednesday</h1>
+			<div id="<?php echo ("view_$page") ?>">&nbsp;</div>
+        </main>
 		<footer>
-
-		</footer>
-
+			<!-- This is a footer. --> 
+			<p>&copy; <?php echo date("Y"); ?> Banana Bunch, All Rights Reserved.</p>
+        </footer>
 		<script>
-			// document.querySelectorAll(".say-hi").forEach(button => {
-			// 	button.addEventListener("click", function() {
-			// 		alert("Hello world!");
-			// 	});
-			// });
+			const request = new XMLHttrequest();
+			request.open("GET", "api/api-test.php", true);
+			request.onload = () => {
+				if (request.status >= 400){return false;}
+				console.log(request);
+				let data = request.response;
+				processResponse(data);
+			};
 
-			document.addEventListener("click", event => {
-				let eventSource = event.target;
+			request.onerror = (event) => {
+				console.log("Error Occured");
+				console.log(event);
+			};
 
-				if (!eventSource.classList.contains("say-hi")) return;
+			function processResponse(data) {
+				data = JSON.parse(data);
+				let text = data.value ?? "NO VALUE!";
+				let output = '<h1>${}</h1>';
+				document.getElementById("view_home").innerHTML = output;
+			}
 
-				let displayText = eventSource.dataset.message ?? "No message found." //Separate content like strings out of logic - maybe a dictionary for no message found message.
-				alert(displayText);
-			});
+			request.send();
 		</script>
-	</body>
+    </body>
 </html>
