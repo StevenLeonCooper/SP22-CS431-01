16135af7ac4dfbba37e0f44b81047c8b8894c0ec
Ryan Patrick <rmoneymail5@gmail.com>
2022-03-05 19:35:46 -0800

    Modified files for phase 1 completeness.

--- index.php
+++ index.php
@@ -21,15 +21,18 @@
                 	echo "Today is " . date("Y/m/d") . "<br>";
 				?>
 			</p>
-			<button class="say-hi" type="button" data-message="Hello world!!">Hello world</button>
+			<button id="view_home"+ class="say-hi" type="button" data-message="Hello world!!">No data received</button>
 		</main>
 		<footer>
 			<!-- This is a footer. --> 
 			<p>&copy; <?php echo date("Y"); ?> Banana Bunch, All Rights Reserved.</p>
         </footer>
 		<script>
-			const request = new XMLHttrequest();
-			request.open("GET", "api/api-test.php", true);
+			// AJAX request object
+			const request = new XMLHttpRequest();
+			request.open("GET", "api/test", true);
+
+			// Callback function that triggers after the request has finished
 			request.onload = () => {
 				if (request.status >= 400){return false;}
 				console.log(request);
@@ -37,15 +40,18 @@
 				processResponse(data);
 			};
 
+			// Callback function that triggers if there's a network error
 			request.onerror = (event) => {
 				console.log("Error Occured");
 				console.log(event);
 			};
 
+			// Processes the request
 			function processResponse(data) {
 				data = JSON.parse(data);
-				let text = data.value ?? "NO VALUE!";
-				let output = '<h1>${}</h1>';
+				console.log("value:" + data.value)
+				let text = data.value ?? "NO VALUE!!";
+				let output = `<h1>${text}</h1>`;
 				document.getElementById("view_home").innerHTML = output;
 			}
 
