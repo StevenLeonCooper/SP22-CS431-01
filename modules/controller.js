bf73258a08a51f6f4972cb0cb339870547bb011a
Ryan Patrick <rmoneymail5@gmail.com>
2022-03-12 23:37:32 -0800

    Added code for controller base class.

--- modules/controller.js
+++ modules/controller.js
@@ -1,7 +1,17 @@
+import {Model} from './model.js';
+import {View} from './view.js';
 
+// Exporting here allows us to reference model and view in individual controllers.
+export {Model, View};
 
 export class Controller {
 	constructor(view, model) {
-		
+		this.view = new View(view);
+		this.model = new Model(model);
+	}
+
+	async setup() {
+		await this.view.setup();
+		await this.model.setup();
 	}
 }
