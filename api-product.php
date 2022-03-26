46a16b7bdc60e0b1922f8c017f063f71b927449f
Jessica Alarcon <35970221+Alarconj1498@users.noreply.github.com>
2022-03-25 20:09:06 -0700

    Update api-product.php

--- api-product.php
+++ api-product.php
@@ -4,16 +4,12 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
 $method = $_SERVER['REQUEST_METHOD']; 
-
 
 if (!function_exists($method)) { 
     header($_SERVER["SERVER_PROTOCOL"] . " 405 Method Not Allowed", true, 405);
     exit;
 }
-
-
 
 require("helpers/mysql_setup.php");
 require("helpers/server.php");
