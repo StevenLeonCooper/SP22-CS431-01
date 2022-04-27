febbfcb3533a4e678889b38ea25c7fadd6b67383
Ryan Patrick <rmoneymail5@gmail.com>
2022-04-26 18:42:05 -0700

    Initial code for user registration.

--- api/api-login.php
+++ api/api-login.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -23,18 +25,46 @@ $response = new Response();
 
 $method($_REQUEST, $db, $response, $put);
 
-function GET($req, PDO $db, $response, $put) {
-   
-}
-
 function POST($req, PDO $db, $response, $put) {
+    try {
+        $postJson = $_POST['json'] ?? false;
 
+        if($postJson){
+            $_POST = json_decode($postJson, true);
+            // keep the 'json' property for backwards compatability
+            $_POST['json'] = $postJson;
+        }
+        
+
+        $params = array(
+            ':username' => $_POST['username'],
+            ':password'  => $_POST['password'],
+            ':email' => $_POST['email'],
+            ':first_name' => $_POST['first_name'],
+            ':last_name' => $_POST['last_name'],
+            ':picture' => $_POST['picture'],
+            ':role_id' => $_POST['role_id']
+        );
+
+        $statement = $db->prepare('CALL create_user(:username,:password,:email,:first_name,:last_name,:picture,:role_id)');
+
+        $statement->execute($params);
+
+        $result = $statement->fetchAll();
+
+        $response->status = "OK";
+    }
+    catch(Exception $error) {
+        $msg = $error->getMessage();
+
+        $result = ["error" => $error->getMessage()];
+
+        $response->status = "FAIL: $msg";
+    }
+
+    $response->outputJSON($result);
 }
 
 function PUT($req, PDO $db, $response, $put) {
 
-}
-
-function DELETE($req, PDO $db, $response, $put) {
-	
 }
