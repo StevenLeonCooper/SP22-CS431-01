b8737032074c8dd5a5f26a4ad0cb7557574fbaa4
Ryan Patrick <rmoneymail5@gmail.com>
2022-05-01 11:18:01 -0700

    Added POST, PUT, and DELETE methods to users api.

--- api/api-user.php
+++ api/api-user.php
@@ -83,3 +83,145 @@ function GET($req, PDO $db, $response)
     }
     $response->outputJSON($result);
 }
+
+function POST($req, PDO $db, $response) 
+{
+    try {
+
+        $perms = new Permissions(1, 1, 1);
+
+        $user = $_SESSION['user'];
+        $userPerms = $user['permissions'];
+    
+        $uri = $_SERVER['REQUEST_URI'];
+        $hasFullAccess = $perms->verify($uri, $userPerms);
+
+        if(!$hasFullAccess) {
+            header($_SERVER["SERVER_PROTOCOL"] . " 403 Forbidden", true, 403);
+            $response = new Response();
+            $response->outputJSON("{}");
+        }
+
+        $postJson = $_POST['json'] ?? false;
+
+        if($postJson){
+            $_POST = json_decode($postJson, true);
+            // keep the 'json' property for backwards compatability
+            $_POST['json'] = $postJson;
+        }
+
+        $hashed_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+        $params = array(
+            ':username' => $_POST['username'],
+            ':password'  => $hashed_password,
+            ':email' => $_POST['email'],
+            ':first_name' => $_POST['first_name'],
+            ':last_name' => $_POST['last_name'],
+        );
+
+        $statement = $db->prepare('CALL create_user(:username,:email,:first_name,:last_name,:password)');
+        
+        $statement->execute($params);
+        
+        $result = $statement->fetchAll();
+
+        $userResult = $result[0];
+        
+        if(isset($userResult['error'])) {
+            throw new Exception($userResult['error']);
+        }
+
+        $response->status = "OK";
+    } catch (Exception $error) {
+        $msg = $error->getMessage();
+
+        $result[0] = ["error" => $error->getMessage()];
+
+        $response->status = "FAIL: $msg";
+    }
+    $response->outputJSON($result);
+}
+
+function PUT($req, PDO $db, $response)
+{
+    $put = [];
+    parse_str(file_get_contents("php://input"), $put);
+    try {
+
+        $perms = new Permissions(1, 1, 1);
+
+        $user = $_SESSION['user'];
+        $userPerms = $user['permissions'];
+    
+        $uri = $_SERVER['REQUEST_URI'];
+        $perms->verify($uri, $userPerms);
+
+        $putJson = $put['json'] ?? false;
+
+        if($putJson){
+            $put = json_decode($putJson, true);
+            // keep the 'json' property for backwards compatability
+            $put['json'] = $putJson;
+        }
+        
+
+        $params = array(
+            ':id' => $put['id'],
+            ':username' => $put['username'],
+            ':email' => $put['email'],
+            ':first_name' => $put['first_name'],
+            ':last_name' => $put['last_name'],
+            ':password'  => $put['password'],
+        );
+
+        $statement = $db->prepare('CALL update_product(:id,:title,:desc,:image,:price,:stock)');
+
+        $statement->execute($params);
+
+        $result = $statement->fetchAll();
+
+        $response->status = "OK";
+
+    }   catch (Exception $error) {
+        $msg = $error->getMessage();
+
+        $result = ["error" => $error->getMessage()];
+
+        $response->status = "FAIL: $msg";
+    }
+
+        $response->outputJSON($result);
+}
+
+function DELETE($req, PDO $db, $response) {
+    try {
+
+        $perms = new Permissions(1, 1, 1);
+
+        $user = $_SESSION['user'];
+        $userPerms = $user['permissions'];
+    
+        $uri = $_SERVER['REQUEST_URI'];
+        $perms->verify($uri, $userPerms);
+
+        $param = $req['id'];
+
+        $statement = $db->prepare("CALL delete_user(?)");
+
+        $statement->execute([$param]);
+
+        $result = $statement->fetchAll();
+
+        $response->status = "OK";
+
+    } catch (Exception $error) {
+        $msg = $error->getMessage();
+
+        $result = ["error" => $error->getMessage()];
+
+        $response->status = "FAIL: $msg";
+    }
+
+    $response->outputJSON();
+}
